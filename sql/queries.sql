be819a9adf1d7c14267371473d36b015c450b887
ArqumJanjua <arqumjanjua@gmail.com>
2024-07-26 02:49:31 +0000

    init SQL

---
+++ sql/queries.sql
@@ -0,0 +1,22 @@
+# Introduction
+
+# SQL Quries
+
+###### Table Setup (DDL)
+
+###### Question 1: You, for some reason, want a combined list of all surnames and all facility names. Yes, this is a contrived example :-). Produce that list! 
+
+```sql
+select surname
+	from cd.members
+union
+select name
+	from cd.facilities;
+```
+
+###### Questions 2: Lorem ipsum...
+
+```sql
+SELECT blah blah 
+```
+
